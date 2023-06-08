d460f435ef675476c691060c71a45f0e94afac4a
YasamanMojtabavi <Myasaman52@gmail.com>
2023-06-08 16:37:21 +0330

    python

--- Assignment_4/instagram new followers finder.py
+++ Assignment_4/instagram new followers finder.py
@@ -1,24 +1,25 @@
 import instaloader
 import getpass
 
+f=open("followers.txt","r")
+old_followers=[]
+for line in f:
+    old_followers.append(line.strip())
+f.close()
+
 L=instaloader.Instaloader()
 
 username=input("please enter username: ")
 password=getpass.getpass("please enter password: ")
+
 L.login(username,password)
 print("hooora!successfuly logged in! ")
 
 profile=instaloader.Profile.from_username(L.context,username)
 
-f=open("followers.txt","r")
-old_followers=[]
-for line in f:
-    old_followers.append(line)
-f.close()
-
 new_followers=[]
 for follower in profile.get_followers():
-    new_followers.append(follower)
+    new_followers.append(follower.username)
 
 for new_follower in new_followers:
     if new_follower not in old_followers:
