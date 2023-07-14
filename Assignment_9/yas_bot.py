8e7584092fd017f28eda2d05d18cfb0d2d9d0817
YasamanMojtabavi <Myasaman52@gmail.com>
2023-07-14 19:56:22 +0330

    Assignment_9

--- Assignment_9/yas_bot.py
+++ Assignment_9/yas_bot.py
@@ -44,6 +44,7 @@ def send_qrcode(message):
 def echo_all(message):
 	if message.text == "hello":
 		bot.send_message(message.chat.id,"Hello")
+		
 	else:
 		bot.send_message(message.chat.id,"icant understand",reply_markup=my_keyboard)
 
