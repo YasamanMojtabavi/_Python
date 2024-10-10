5adabdfcfbca1b98e531c7583ad1aca2e27310a3
YasamanMojtabavi <Myasaman52@gmail.com>
2024-10-10 17:08:37 +0330

    python

--- Assignment_10/fraction.py
+++ Assignment_10/fraction.py
@@ -10,32 +10,47 @@ class kasr:
         self.makhraj=int(input("The denominator cannot be zero. Enter a Digit number:"))
      
     def multi(self,s,m):
-         self.soorat=self.soorat*s
-         self.makhraj=self.makhraj*m
+         soorat=self.soorat*s
+         makhraj=self.makhraj*m
+         print(soorat ,"/" , makhraj)
 
     def sum(self,s,m):
         if self.makhraj==m:
-            self.soorat+=s
+            soorat=self.soorat+s
+            makhraj=m
         else:
-            self.soorat=(self.soorat*m)+(s*self.makhraj)
-            self.makhraj*=m
+            soorat=(self.soorat*m)+(s*self.makhraj)
+            makhraj=self.makhraj*m
+        print(soorat ,"/" , makhraj)
 
     def divis(self,s,m):
-        self.soorat=self.soorat*m
-        self.makhraj=self.makhraj*s
+        soorat=self.soorat*m
+        makhraj=self.makhraj*s
+        print(soorat ,"/" , makhraj)
 
     def sub(self,s,m):
         if self.makhraj==m:
-            self.soorat-=s
+            soorat=self.soorat-s
+            makhraj=m
         else:
-            self.soorat=(self.soorat*m)-(s*self.makhraj)
-            self.makhraj*=m
-        
+            soorat=(self.soorat*m)-(s*self.makhraj)
+            makhraj=self.makhraj*m
+        print(soorat ,"/" , makhraj)
 
-my_kasr1=kasr(3,6)
-my_kasr2=kasr(2,6)
-#my_kasr1.sub(my_kasr2.soorat,my_kasr2.makhraj)
-#my_kasr1.divis(my_kasr2.soorat,my_kasr2.makhraj)
-#my_kasr1.sum(my_kasr2.soorat,my_kasr2.makhraj)
-my_kasr1.multi(my_kasr2.soorat,my_kasr2.makhraj)
-print(my_kasr1.soorat , "/" , my_kasr1.makhraj)
+
+s_1=int(input("please enter numerator of fraction1:"))
+m_1=int(input("please enter denominator of fraction1:"))
+s_2=int(input("please enter numerator of fraction2:"))
+m_2=int(input("please enter denominator of fraction2:"))
+my_kasr1=kasr(s_1,m_1)
+my_kasr2=kasr(s_2,m_2)
+choice=input("Enter your choice:sub, divis, sum, multi: ")
+if choice=="sub":
+    my_kasr1.sub(my_kasr2.soorat,my_kasr2.makhraj)
+if choice=="divis":
+    my_kasr1.divis(my_kasr2.soorat,my_kasr2.makhraj)
+if choice=="sum":
+    my_kasr1.sum(my_kasr2.soorat,my_kasr2.makhraj)
+if choice=="multi":
+    my_kasr1.multi(my_kasr2.soorat,my_kasr2.makhraj)
+
